63902e5d0eb4812780956746d5ce6cfedb5332a7
Buker, Nick <Nick.Buker@nordstrom.com>
2017-09-15 17:01:23 -0700

    remove storing of data

--- DistanceClassifier.py
+++ DistanceClassifier.py
@@ -14,46 +14,56 @@ class DistanceClassifier(object):
         """
         Call _find_centroids to locate the centroids of each class
 
-        Input: X_train (pandas Dataframe or numpy array) data,
+        Input:
+        X_train (pandas Dataframe or numpy array) data,
         y_train (pandas Series or numpy array) labels
-        Returns: none
-        """
-        self.X_train = X_train
-        self.y_train = y_train
-        self._cent_map, self._centroids = self._find_centroids()
 
-    def _find_centroids(self):
+        Returns:
+        None
+        """
+        self._cent_map, self._centroids = self._find_centroids(X_train, y_train)
+
+    def _find_centroids(self, X_train, y_train):
         """
         Locate the centroids of each class
 
-        Input: none
-        Returns: cent_map (dictionary with column names:labels),
+        Input:
+        X_train (pandas Dataframe or numpy array) data,
+        y_train (pandas Series or numpy array) labels
+
+        Returns:
+        cent_map (dictionary with column names:labels),
         centroids (list of pandas Series) coordinates of centroids
         """
         centroids = []
         cent_map = {}
-        for i, c in enumerate(self.y_train.unique()):
-            centroids.append(np.mean(self.X_train[self.y_train == c]))
+        for i, c in enumerate(y_train.unique()):
+            centroids.append(np.mean(X_train[y_train == c]))
             cent_map['c{}'.format(i)] = c
         return cent_map, centroids
 
     def predict(self, X_test):
         """
-        Call _find_distances and _assign_points to classify test data
+        Calls _find_distances and _assign_points to classify test data
 
-        Input: X_test (pandas DataFrame or numpy array) data
-        Returns: pandas Series of assignments
+        Input:
+        X_test (pandas DataFrame or numpy array) data
+
+        Returns:
+        pandas Series of assignments
         """
-        self.X_test = X_test
-        distances = self._find_distances()
+        distances = self._find_distances(X_test)
         return self._assign_points(distances)
 
-    def _find_distances(self):
+    def _find_distances(self, X_test):
         """
         Calculate euclidean distance between the centroids and data points
 
-        Input: none
-        Returns: pandas DataFrame with distances to each centroid
+        Input:
+        X_test (pandas Dataframe or numpy array) data
+
+        Returns:
+        pandas DataFrame with distances to each centroid
         """
         # wrap euclidean distance to work with apply
         def euc(series):
@@ -61,16 +71,18 @@ class DistanceClassifier(object):
 
         distances = pd.DataFrame()
         for i, c in enumerate(self._centroids):
-            distances['c{}'.format(i)] = self.X_test.apply(euc, axis=1)
-        self.distances = distances
+            distances['c{}'.format(i)] = X_test.apply(euc, axis=1)
         return distances
 
     def _assign_points(self, distances):
         """
         Use distances and _cent_map to assign labels to points
 
-        Input: distances to each centroid (pandas DataFrame)
-        Returns: pandas Series of assignments
+        Input:
+        distances to each centroid (pandas DataFrame)
+
+        Returns:
+        pandas Series of assignments
         """
         cent_cols = distances.idxmin(axis=1)
         return pd.Series([self._cent_map[p] for p in cent_cols])
